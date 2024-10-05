5ae6b0dc2b4e5fcbba222018812ebe00894657d4
azevedorar <lucas.timmy1@gmail.com>
2024-10-05 15:18:55 -0300

    updatee2

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -7,7 +7,8 @@ import { FaDownload } from "react-icons/fa";
 const Home: React.FC = () => {
   return (
     <div className="home-container">
-      <h1>LUCAS SANTOS</h1>
+      <h1>LUCAS SANTOS </h1>
+      
       <div className="button-container">
         <Link to="/portfolio/musical-portfolio" className="button">
           Produtor Musical
@@ -24,6 +25,15 @@ const Home: React.FC = () => {
         fiz esse site para vocês não terem mais desculpas <br />
         dizendo que nao tenho experiência!
       </p>
+
+      {/* Rodapé */}
+      <footer className="footer">
+        <h2>
+          <a href="https://github.com/azevedorar" target="_blank" rel="noopener noreferrer">
+            github.com/azevedorar
+          </a>
+        </h2>
+      </footer>
     </div>
   );
 };
