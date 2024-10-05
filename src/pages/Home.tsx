f993c71815e2dd1d28958abe9079d4548e49d891
azevedorar <lucas.timmy1@gmail.com>
2024-10-05 14:54:41 -0300

    updatee

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,14 +1,12 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import "./Home.css"; // Importe o CSS para estilização
+import "./Home.css"; 
 import '../animation.css';
 import { FaDownload } from "react-icons/fa";
-
 
 const Home: React.FC = () => {
   return (
     <div className="home-container">
-      
       <h1>LUCAS SANTOS</h1>
       <div className="button-container">
         <Link to="/portfolio/musical-portfolio" className="button">
@@ -18,15 +16,14 @@ const Home: React.FC = () => {
           Desenvolvedor & Cyber
         </Link>
         <a href="./CV_LUCAS_SANTOS.pdf" download className="button">
-        <FaDownload />
+          <FaDownload />
         </a>
-        
       </div>
-      <br />
-      <br />
-      <br />
-      <p>fiz esse site para vocês não terem mais desculpas <br />
-      dizendo que nao tenho experiência!</p>
+
+      <p>
+        fiz esse site para vocês não terem mais desculpas <br />
+        dizendo que nao tenho experiência!
+      </p>
     </div>
   );
 };
--- src/pages/ProgrammingPortfolio.tsx
+++ src/pages/ProgrammingPortfolio.tsx
@@ -1,24 +1,17 @@
 import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import "./ProgrammingPortfolio.css";
-import { FaArrowLeft, FaChevronDown, FaChevronUp } from "react-icons/fa";
+import { FaArrowLeft, FaChevronDown, FaChevronUp,  } from "react-icons/fa";
+
 
 const ProgrammingPortfolio: React.FC = () => {
-  // Estados para controlar cada dropdown
   const [experienciasOpen, setExperienciasOpen] = useState(false);
   const [habilidadesOpen, setHabilidadesOpen] = useState(false);
   const [certificadosOpen, setCertificadosOpen] = useState(false);
-  // Função para alternar o dropdown
-  const toggleExperiencias = () => {
-    setExperienciasOpen(!experienciasOpen);
-  };
-  const toggleHabilidades = () => {
-    setHabilidadesOpen(!habilidadesOpen);
-  };
 
-  const toggleCertificados = () => {
-    setCertificadosOpen(!certificadosOpen);
-  };
+  const toggleExperiencias = () => setExperienciasOpen(!experienciasOpen);
+  const toggleHabilidades = () => setHabilidadesOpen(!habilidadesOpen);
+  const toggleCertificados = () => setCertificadosOpen(!certificadosOpen);
 
   return (
     <div className="page-content-Programming">
@@ -30,146 +23,149 @@ const ProgrammingPortfolio: React.FC = () => {
         Cyber Security
       </h1>
 
-      {/* Dropdown Experiências */}
       <div className="dropdown-section">
-        <div className="dropdown-header" onClick={toggleExperiencias}>
+        <div
+          className="dropdown-header"
+          onClick={toggleExperiencias}
+          aria-expanded={experienciasOpen}
+        >
           <h2>Experiências</h2>
           {experienciasOpen ? <FaChevronUp /> : <FaChevronDown />}
         </div>
         {experienciasOpen && (
           <div className="dropdown-content">
-            <h3> Vivo (Telefonica Brasil)</h3>
-            <h2>Analista em Segurança da Informação Jr • 2022 - 2024</h2>
-            <p>
-              Atuei com os times de Cloud e Arquitetura, adquirindo
-              conhecimentos em CIS, Owasp, AWS, GCP, Huawei Cloud e Microsoft
-              Azure. Após ser efetivado, colaborei em pesquisas sobre a
-              arquitetura da empresa e estratégias de segurança em um modelo
-              zero trust. Organizei informações em uma wiki interna, facilitando
-              a comunicação entre as áreas. Implementei Power BI em uma página
-              interativa, usando HTML, CSS e JavaScript, e gerenciei conteúdos
-              no Microsoft Confluence e SharePoint. Utilize o ServiceNow para
-              atender chamados de instalação de software, analisando
-              vulnerabilidades e verificando riscos segundo o NIST. Após
-              aprovação, os aplicativos eram adicionados à nossa central de
-              softwares interna, permitindo que os colaboradores realizassem a
-              instalação.
-            </p>
-            <h3> Telefonica Educação Digital</h3>
-            <h2>Estagiário Help Desk • 2021 - 2022</h2>
-            <p>
-              Criação e manutenção de usuários, computadores e grupos utilizando
-              o Active Directory (AD). Utilizando OCS Inventory para gerenciar
-              computadores. Instalação, configuração e manutenção de softwares
-              da plataforma Office 365. Atendimento de colaboradores através da
-              ferramenta de tickets Redmine. E manutenção de computadores via
-              Teamviewer
-            </p>
-            <h3> Phooto Brasil</h3>
-            <h2>Estagiário de Suporte Técnico • Jun 2018 - Dez 2018</h2>
-            <p>
-              Fui designado para arrumar hardwares e softwares de máquinas e
-              também configurando a rede interna da empresa
-            </p>
-
-            {/* Adicione mais experiências aqui */}
+            <h2> Vivo (Telefonica Brasil)</h2>
+            <h3>
+              Analista em Segurança da Informação Jr
+              <br />
+              2022 - 2024
+            </h3>
+            {/* <p> Descrição da experiência... </p> */}
+            <h2> Telefonica Educação Digital</h2>
+            <h3>
+              Estagiário Help Desk <br />
+              2021 - 2022
+            </h3>
+            {/* <p> Descrição da experiência... </p> */}
+            <h2> Phooto Brasil</h2>
+            <h3>
+              Estagiário de Suporte Técnico <br /> Jun 2018 - Dez 2018
+            </h3>
+            {/* <p> Descrição da experiência... </p> */}
           </div>
         )}
       </div>
 
       <div className="dropdown-section">
-        <div className="dropdown-header" onClick={toggleHabilidades}>
+        <div
+          className="dropdown-header"
+          onClick={toggleHabilidades}
+          aria-expanded={habilidadesOpen}
+        >
           <h2>Habilidades</h2>
           {habilidadesOpen ? <FaChevronUp /> : <FaChevronDown />}
         </div>
         {habilidadesOpen && (
-          <div className="dropdown-content">
-            <h3>Principais habilidades</h3>
-            <p>
-              HTML 5 (intermediário)  <br />
-              CSS (Intermediário) <br />
-              JavaScript (Intermediário) <br />
-              React.js (Básico) <br />
-              Node.Js (básico) <br />
-              Vite (Básico)<br />
-              TailWind CSS (Básico) <br />
-              ChatGPT (Avançado) <br />
-              Adobe Photoshop (básico)<br />
-              ServiceNow (básico) <br />
-              Jira (Básico) <br />
-              PowerBI (Intermediário) <br />
-              Python (básico) <br />
-              Kali Linux (básico) <br />
-              Microsoft Visio (básico) <br />
-              Microsoft Sharepoint (Intermediário) <br />
-              OWASP (Básico) <br />
-              Redmine ( Básico) <br />
-              Office 365 (Intermediário)<br />
-              Typescript (Intermediário) <br />
-              Vite (Intermediário) <br />
-            </p>
+          <div className="dropdown-content-skills">
+            {/* <h2>Principais habilidades</h2> */}
+            <h3>HTML 5 - intermediário</h3>  
+            <h3>CSS - intermediário</h3>
+            <h3>JavaScript - intermediário</h3>
+            <h3>React.js - Básico</h3>
+            <h3>Node.Js - Básico</h3>
+            <h3>Vite - Básico</h3>
+            <h3>TailWind CSS - Básico</h3>
+            <h3>ChatGPT - Avançado</h3>
+            <h3>Adobe Photoshop - Básico</h3>
+            <h3>ServiceNow - Básico</h3>
+            <h3>Jira - Básico</h3>
+            <h3>PowerBI - intermediário</h3>
+            <h3>Python - Básico</h3>
+            <h3>Kali Linux - Básico</h3>
+            <h3>Microsoft Visio - Básico</h3>
+            <h3>Microsoft Sharepoint - intermediário</h3>
+            <h3>OWAS - Básico</h3>
+            <h3>Redmine - Básico</h3>
+            <h3>Office 365 - intermediário</h3>
+            <h3>Typescript - intermediário</h3>
+            <h3>Vite - intermediário</h3>
           </div>
         )}
       </div>
 
       <div className="dropdown-section">
-        <div className="dropdown-header" onClick={toggleCertificados}>
+        <div
+          className="dropdown-header"
+          onClick={toggleCertificados}
+          aria-expanded={certificadosOpen}
+        >
           <h2>Certificados</h2>
           {certificadosOpen ? <FaChevronUp /> : <FaChevronDown />}
         </div>
         {certificadosOpen && (
           <div className="dropdown-content">
-            <h3>Principais Certificações</h3>
-            <p>
+            <div className="certificados">
+
+            <h1>Principais Certificações</h1>
+            <h3>
               <a
                 href="https://drive.google.com/file/d/1vNPKgtSErgjUG9MkxhoRzH5lWjj8EWAK/view"
                 target="_blank"
                 rel="noopener noreferrer"
               >
-                - API DESIGN (API ACADEMY) <br />
+                API DESIGN (API ACADEMY)
               </a>
+            </h3>
+            <h3>
               <a
                 href="https://drive.google.com/file/d/1TywcSviJbOEGCUAyDMNRM7tZg6du5nXr/view"
                 target="_blank"
                 rel="noopener noreferrer"
               >
-                - CyberSecurity (FIAP) <br />
+                CyberSecurity (FIAP)
               </a>
+            </h3>
+            <h3>
               <a
                 href="https://www.sp.senai.br/consulta-certificado?qrcode=00013113/6906163"
                 target="_blank"
                 rel="noopener noreferrer"
               >
-                - LGPD (SENAI) <br />
+                LGPD (SENAI)
               </a>
+            </h3>
+            <h3>
               <a
                 href="https://on.fiap.com.br/pluginfile.php/1/local_nanocourses/certificado_nanocourse/66047/e82daadf98311d01f1caa4f6b2ee5654/certificado.png"
                 target="_blank"
                 rel="noopener noreferrer"
               >
-                - Cloud Fundamentals, Administration and Solution Architect
-                (FIAP) <br />
+                Cloud Fundamentals, Administration and Solution Architect (FIAP)
               </a>
+            </h3>
+            <h3>
               <a
                 href="https://www.schoolofnet.com/validar-certificado/?certificate=1ca04bb0-c99b-468b-ab65-e7979ffc65c1"
                 target="_blank"
                 rel="noopener noreferrer"
               >
-                - Javascript <br />
+                Javascript
               </a>
+            </h3>
+            <h3>
               <a
                 href="https://www.schoolofnet.com/validar-certificado/?certificate=00c136f7-6d72-42cc-ae4e-87af291e671a"
                 target="_blank"
                 rel="noopener noreferrer"
               >
-                - SASS
+                SASS
               </a>
-            </p>
+            </h3>
+            </div>
+            
           </div>
         )}
       </div>
-
-      {/* Você pode adicionar mais dropdowns para outros tópicos */}
     </div>
   );
 };
